a6ef0841aae5ca7eeaf9f29cf28789a2f517c18a
Edgar Ayala <47013936+Escobar512@users.noreply.github.com>
2023-03-06 14:40:40 -0600

    stop added

--- assets/js/test.js
+++ assets/js/test.js
@@ -6,9 +6,6 @@ $(document).ready(function () {
     const scanner = new Html5QrcodeScanner(
         "qr-reader", { facingMode: { exact: "environment" } }, { fps: 10, qrbox: 250 });
 
-
-
-
     function onscansuccess(qrcodemessage) {
         window.location.href = qrcodemessage;
         console.log(qrcodemessage);
@@ -18,25 +15,24 @@ $(document).ready(function () {
 
     const config = { fps: 10, qrbox: 250 }
 
-    $("#testButton").click(function () {
-
-        const html5QrCode = new Html5Qrcode("qr-reader");
+    const html5QrCode = new Html5Qrcode("qr-reader");
         const qrCodeSuccessCallback = (decodedText, decodedResult) => {
             /* handle success */
         };
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
-        html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+    $("#testButton").click(function () {
+
+        
+        const config2 = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+        html5QrCode.start({ facingMode: "environment" }, config2, qrCodeSuccessCallback);
     });
 
 
     $("#testButton2").click(function () {
 
-        Html5Qrcode.stop().then((ignore) => {
-            // QR Code scanning is stopped.
-        }).catch((err) => {
-            // Stop failed, handle it.
-        });
+        html5QrCode.stop({ facingMode: "environment" }, config2, qrCodeSuccessCallback);
     });
 
 
