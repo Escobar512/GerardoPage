444a988afe21375b43cacf9f2b9a85f8600cfbaf
Edgar Ayala <47013936+Escobar512@users.noreply.github.com>
2023-03-06 14:36:47 -0600

    manual scanner

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,16 +1,50 @@
-var scanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
 
-scanner.render(onscansuccess);
 
-function onscansuccess(qrcodemessage) {
-    window.location.href = qrcodemessage;
-    console.log(qrcodemessage);
-}
+$(document).ready(function () {
 
-$("#testButton").click(function () {
 
-    
-    modalScanner();
-    scanner.start();
+    const scanner = new Html5QrcodeScanner(
+        "qr-reader", { facingMode: { exact: "environment" } }, { fps: 10, qrbox: 250 });
+
+
+
+
+    function onscansuccess(qrcodemessage) {
+        window.location.href = qrcodemessage;
+        console.log(qrcodemessage);
+    }
+
+    scanner.render(onscansuccess);
+
+    const config = { fps: 10, qrbox: 250 }
+
+    $("#testButton").click(function () {
+
+        const html5QrCode = new Html5Qrcode("qr-reader");
+        const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+            /* handle success */
+        };
+        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+        html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+    });
+
+
+    $("#testButton2").click(function () {
+
+        Html5Qrcode.stop().then((ignore) => {
+            // QR Code scanning is stopped.
+        }).catch((err) => {
+            // Stop failed, handle it.
+        });
+    });
+
+
+
 });
+
+
+
+
+
+
